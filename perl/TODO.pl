352b57da2e697a2a1fc779b0937b0eb10662a534
whymaths <whymaths@gmail.com>
2012-08-17 11:33:55 +0800

    proftpd.conf

---
+++ perl/TODO.pl
@@ -0,0 +1,42 @@
+#!/usr/bin/perl 
+#===============================================================================
+#
+#         FILE: TODO.pl
+#
+#        USAGE: ./TODO.pl  
+#
+#  DESCRIPTION: 
+#
+#      OPTIONS: ---
+# REQUIREMENTS: ---
+#         BUGS: ---
+#        NOTES: ---
+#       AUTHOR:  (), |whymaths@gmail.com|
+#      COMPANY: 
+#      VERSION: 1.0
+#      CREATED: 08/13/2012 01:54:51 PM
+#     REVISION: ---
+#===============================================================================
+
+use strict;
+use warnings;
+
+
+
+BEGIN {
+    push (@INC. '/opt/perl/lib');
+}
+
+BEGIN {
+    unshift @INC, '/opt/perl/lib';
+}
+
+
+use lib '/opt/perl/lib';
+
+
+use FindBin qw($Bin);
+
+use lib "$Bin/../lib";
+
+
